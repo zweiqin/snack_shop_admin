8c0ea041171172cef0cb088b128f6aebd5ac1978
zweiqin <zweiqin88@163.com>
2022-11-28 14:36:18 +0800

    2022.11.28 - 零食项目的商户后台，订单管理页面增加配送时间显示（零食商城后台每个shipping_type为1配送时，显示订单的deliveryTime配送时间）

--- src/pages/order/orderList/components/tableExpand.vue
+++ src/pages/order/orderList/components/tableExpand.vue
@@ -14,20 +14,26 @@
                 <span class="expand-value" v-text="row.spread_nickname?row.spread_nickname:'无'"></span>
             </Col>
         </Row>
-        <Row>
-            <Col span="8">
-                <span class="expand-key">用户备注：</span>
-                <span class="expand-value" v-text="row.mark?row.mark:'无'"></span>
-            </Col>
-            <Col span="8">
-                <span class="expand-key">商家备注：</span>
-                <span class="expand-value" v-text="row.remark?row.remark:'无'"></span>
-            </Col>
-            <Col span="8">
-                <span class="expand-key">核销码：</span>
-                <span class="expand-value" v-text="row.verify_code?row.verify_code:'无'"></span>
-            </Col>
-        </Row>
+      <Row class="expand-row">
+        <Col span="8">
+          <span class="expand-key">用户备注：</span>
+          <span class="expand-value" v-text="row.mark?row.mark:'无'"></span>
+        </Col>
+        <Col span="8">
+          <span class="expand-key">商家备注：</span>
+          <span class="expand-value" v-text="row.remark?row.remark:'无'"></span>
+        </Col>
+        <Col span="8">
+          <span class="expand-key">核销码：</span>
+          <span class="expand-value" v-text="row.verify_code?row.verify_code:'无'"></span>
+        </Col>
+      </Row>
+      <Row v-if="row.shipping_type==1">
+        <Col span="8">
+          <span class="expand-key">配送时间：</span>
+          <span class="expand-value" v-text="row.deliveryTime?row.deliveryTime:'无'"></span>
+        </Col>
+      </Row>
     </div>
 </template>
 
